85056c930f8897ddddde29fab4246ded8b331587
Carlos Tolosa <carloswtolosa@gmail.com>
2022-11-14 17:31:59 -0300

    Modificando los templates

--- app_mvt/urls.py
+++ app_mvt/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from app_mvt.views import *
 
 urlpatterns = [
-    
+    path("", inicio, name="app-inicio")
 ]
--- app_mvt/views.py
+++ app_mvt/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from app_mvt.models import *
 from app_mvt.forms import *
 # Create your views here.
+
+
+def inicio(request):
+    return render(request, "app_mvt/index.html")
