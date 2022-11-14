768a01e1c9630e838a4700538ec850198038e6ca
Carlos Tolosa <carloswtolosa@gmail.com>
2022-11-14 16:57:58 -0300

    Primer commit del desafio: Subiendo el desafio a github

---
+++ app_mvt/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from app_mvt.views import *
+
+urlpatterns = [
+    
+]
---
+++ app_mvt/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from app_mvt.models import *
+from app_mvt.forms import *
+# Create your views here.
